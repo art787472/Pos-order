e12ea9141a6635c6b73ffe33840debfae096bf87
Chen Yi-Cheng <art787472@gmail.com>
2024-05-28 15:09:37 +0800

    完成策略模式

---
+++ Pos點餐/DiscountContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pos點餐
+{
+    internal class DiscountContext
+    {
+        private ADiscount discount;
+
+        public DiscountContext(string discountOpt)
+        {
+            Type t = Type.GetType(discountOpt);
+            this.discount = (ADiscount)Activator.CreateInstance(t);
+        }
+
+        public List<Item> GetResult(List<Item> items)
+        {
+            return this.discount.ComputeDiscount(items);
+        }
+    }
+}
